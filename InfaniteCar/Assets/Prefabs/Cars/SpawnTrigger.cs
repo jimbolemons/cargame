43caa4e374a029bdb49f1664b8770e6643935f00
jacob drummond <drummond.jacob13@gmail.com>
2019-11-01 16:00:08 -0400

    working on trigger to spawn more enemies

---
+++ InfaniteCar/Assets/Prefabs/Cars/SpawnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag != "Player") return;
+        AICarsManager.policeSpawned = false;
+
+        Debug.Log("spawning new enemy......." +AICarsManager.policeSpawned);
+    }
+}
