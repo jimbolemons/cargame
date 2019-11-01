c573d5119ee51742915bf0df7419313ca0e0dcb3
jacob drummond <drummond.jacob13@gmail.com>
2019-11-01 11:50:30 -0400

    working on rockets

---
+++ InfaniteCar/Assets/scripts/Orbit.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+
+    public GameObject cube;
+    public Transform center;
+    public bool orbitSwitch;
+    public Vector3 axis = Vector3.right;    
+    public Vector3 desiredPosition;
+    public float radius;
+    public float radius2;
+    public float radiusSpeed;
+    public float rotationSpeed = 80.0f;
+
+    void Start()
+    {
+        //cube = GameObject.FindWithTag("Cube");
+        center = cube.transform;       
+        
+        //radius = .5f;
+        if (orbitSwitch)
+        { transform.position = (transform.position - center.position).normalized * -radius + center.position; }
+        if (!orbitSwitch)
+        { transform.position = (transform.position - center.position).normalized * radius + center.position; }
+
+        }
+
+    void Update()
+    {
+        
+       
+            transform.RotateAround(center.position, axis, rotationSpeed * Time.deltaTime);
+            desiredPosition = (transform.position - center.position).normalized * radius2 + center.position;
+            transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * radiusSpeed);
+        
+        
+           
+
+        
+    }
+}
