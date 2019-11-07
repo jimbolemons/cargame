f0a3c0d152a2cad29a2361cc72441c8b9b3dc7aa
jacob drummond <drummond.jacob13@gmail.com>
2019-11-07 13:42:45 -0500

    working on speed streaks

---
+++ InfaniteCar/Assets/scripts/randomOnOff.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class randomOnOff : MonoBehaviour
+{
+    private ParticleSystem particle;
+    bool on = true;
+
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (on)
+        {
+
+        }
+        else if (!on)
+        {
+
+        }
+
+        
+    }
+
+    void Play()
+    {
+        particle.Play();
+        particle.enableEmission = true;
+        
+        
+    }
+    void Stop()
+    {
+        particle.Stop();
+        particle.enableEmission = false;
+    }
+}
