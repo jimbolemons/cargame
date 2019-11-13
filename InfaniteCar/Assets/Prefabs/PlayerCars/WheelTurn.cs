cbcffa06e4a2c3a012355de8f1de5634981567a1
jacob drummond <drummond.jacob13@gmail.com>
2019-11-13 14:51:21 -0500

    fixed wheel turn

--- InfaniteCar/Assets/Prefabs/PlayerCars/WheelTurn.cs
+++ InfaniteCar/Assets/Prefabs/PlayerCars/WheelTurn.cs
@@ -20,33 +20,32 @@ public class WheelTurn : MonoBehaviour
     {
         Scene currentScene = SceneManager.GetActiveScene();
         string sceneName = currentScene.name;
-        float tiltAroundZ = Input.GetAxis("Horizontal") ;
-       // Debug.Log(tiltAroundZ);
+        float tiltAroundZ = Input.GetAxis("Horizontal");
+        transform.localRotation = Quaternion.Slerp(this.transform.localRotation, Quaternion.Euler(0, 0, tiltAroundX), Time.deltaTime * smooth);
+        // Debug.Log(tiltAroundZ);
         if (sceneName == "MainScene")
         {
+           
             if (input.Right())
             {
                 //Debug.Log("Right1"); // this is getting called 
                 tiltAroundX = 1 * tiltAngle; // why the fuck does this not work, it is identical to the left fucntion but does nothing
                 //Debug.Log("Right2");// this is getting called 
-            }
-            else{
+            }else if (input.Left())
+            {
+                //Debug.Log("left");
+                tiltAroundX = -1 * tiltAngle;
+            }          
+            else
+            {
                // Debug.Log("not Right");
 
                 tiltAroundX = 0 * tiltAngle;
             }
 
-            if (input.Left())
-            {
-                //Debug.Log("left");
-                tiltAroundX = -1 * tiltAngle;
-            }else{
-               // Debug.Log("not left");
-
-                tiltAroundX = 0 * tiltAngle;
-            }
+           
         }
-        transform.localRotation = Quaternion.Slerp(this.transform.localRotation, Quaternion.Euler(0,0 , tiltAroundX), Time.deltaTime * smooth);
+        
 
 
     }
